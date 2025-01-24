30c636c9a71e98f09a0fc179168c8ba83b29b889
bt-nghia <97080122+bt-nghia@users.noreply.github.com>
2025-01-24 21:12:40 +0700

    Update train.py to cpu IDS

--- train.py
+++ train.py
@@ -263,7 +263,7 @@ def get_metrics(metrics, grd, pred, topks):
     tmp = {"recall": {}, "ndcg": {}}
     for topk in topks:
         _, col_indice = torch.topk(pred, topk)
-        batch_rec_ids.append(col_indice)
+        batch_rec_ids.append(col_indice.to("cpu"))
         row_indice = torch.zeros_like(col_indice) + torch.arange(pred.shape[0], device=pred.device, dtype=torch.long).view(-1, 1)
         is_hit = grd[row_indice.view(-1).to(grd.device), col_indice.view(-1).to(grd.device)].view(-1, topk)
 
